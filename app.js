f87d58acf9e18c9fa5fa383e2f13f85d30feb318
indrani321 <131009169+indrani321@users.noreply.github.com>
2023-05-23 21:36:26 +0530

    Request/Response done

--- app.js
+++ app.js
@@ -3,7 +3,39 @@ const http=require('http');
 
 
 const server= http.createServer((req,res)=>{
-    console.log("Indrani Bhattacharjee")
+    const url=req.url;
+    if(url==='/home'){
+        res.setHeader('content-type','text/html')
+        res.write('<html>')
+        res.write('<head><title>first response page</title></head>')
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('</html>')
+       return res.end(); 
+    }
+     if(url==='/about'){
+        res.setHeader('content-type','text/html')
+        res.write('<html>')
+        res.write('<head><title>first response page</title></head>')
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</html>')
+       return res.end(); 
+    }
+    if(url==='/node')
+    {
+        res.setHeader('content-type','text/html')
+        res.write('<html>')
+        res.write('<head><title>first response page</title></head>')
+        res.write('<body><h1>Welcome to my node JS project</h1></body>')
+        res.write('</html>')
+       return res.end()
+    }
+    
+    res.setHeader('content-type','text/html')
+    res.write('<html>')
+    res.write('<head><title>first response page</title></head>')
+    res.write('<body><h1>Welcome to my Node JS project</h1></body>')
+    res.write('</html>')
+    res.end();
 });
 
 
